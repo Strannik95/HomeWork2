77292efe0dbd53d45a1d0e1be4e4e703921f0e38
Manap <Manap1995@mail.ru>
2022-08-02 21:52:59 +0300

    Домашнее задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-     //Задание 1
-      byte age = 27;
-       short height = 178;
-       int weight = 94;
-       long money = 1300L;
-       float brotherHeight = 182.3f;
-       double brotherWeight = 86.400;
-       char svo = 'Z';
-       boolean normalHeight = height > 162;
+        //Задание 1
+        byte age = 27;
+        short height = 178;
+        int weight = 94;
+        long money = 1300L;
+        float brotherHeight = 182.3f;
+        double brotherWeight = 86.400;
+        char svo = 'Z';
+        boolean normalHeight = height > 162;
 
-       //Задание 2
+        //Задание 2
 
-       double boxer1 = 78.2;
-       double boxer2 = 82.7;
-       double totalWeight = boxer1 + boxer2;
-       double differenceWeight = boxer2 - boxer1;
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double totalWeight = boxer1 + boxer2;
+        double differenceWeight = boxer2 - boxer1;
         System.out.println("Общий вес боксеров " + totalWeight + " кг");
         System.out.println("Разница в весе боксеров " + differenceWeight + " кг");
 
         //Задание 3
 
-      int bananas = 5;
-      int milk = 200;
-      int plombir = 2;
-      int eggs = 4;
-      bananas = 5 * 80;
-      milk = 200 / 100 * 105;
-      plombir = 2 * 100;
-      eggs = 4 * 70;
+        int bananas = 5;
+        int milk = 200;
+        int plombir = 2;
+        int eggs = 4;
+        bananas = 5 * 80;
+        milk = 200 / 100 * 105;
+        plombir = 2 * 100;
+        eggs = 4 * 70;
 
-     double sportBreakfast = bananas + milk + plombir + eggs;
-      System.out.println("Количество грамм завтрака " + sportBreakfast);
-     double kilogramm = sportBreakfast / 1000;
-     System.out.println("Переведенное в КГ " + kilogramm);
+        double sportBreakfast = bananas + milk + plombir + eggs;
+        System.out.println("Количество грамм завтрака " + sportBreakfast);
+        double kilogramm = sportBreakfast / 1000;
+        System.out.println("Переведенное в КГ " + kilogramm);
 
-     // Задание 4
-      int weight1 = 7;
-      weight1 = 7 * 1000;
-      int diet1 = weight1 / 250;
-      int diet2 = weight1 / 500;
-      int averageDays = (diet1 + diet2) / 2;
-     System.out.println("Если спортсмен будет терять в день 250 гр " + diet1);
-     System.out.println("Если спортсмен будет терять в день 500 гр " + diet2);
-     System.out.println("Среднее количетсво дней " + averageDays);
+        // Задание 4
+        int weight1 = 7;
+        weight1 = 7 * 1000;
+        int diet1 = weight1 / 250;
+        int diet2 = weight1 / 500;
+        int averageDays = (diet1 + diet2) / 2;
+        System.out.println("Если спортсмен будет терять в день 250 гр " + diet1);
+        System.out.println("Если спортсмен будет терять в день 500 гр " + diet2);
+        System.out.println("Среднее количетсво дней " + averageDays);
 
-     // Задание 5
+        // Задание 5
         int masha = 67760;
         int denis = 83690;
         int kris = 76230;
